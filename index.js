4677100e6e75a70fb7713c8006a47120c752bc97
jdai1 <julian.dai@gmail.com>
2023-07-13 21:43:16 -0400

    feat: specify channel and split long messages

--- index.js
+++ index.js
@@ -17,13 +17,34 @@ const openai = new OpenAIApi(configuration);
 
 client.on("messageCreate", async (message) => {
   try {
-    if (message.author.bot) return;
+    if (message.author.bot || message.author.system) return;
+    if (message.channel.name !== "test") return;
+
     const chatCompletion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: message.content }],
     });
-    console.log(chatCompletion.data.choices);
-    message.reply(`${chatCompletion.data.choices[0].message.content}`)
+    console.log('chatCompletion finished');
+    const response = chatCompletion.data.choices[0].message.content;
+    if (response.length > 2000) {
+      var chunk = ''
+      var hasCode = false;
+      const stream = response.split('\n');
+      for (const i in stream) {
+        if (chunk.length + stream[i].length > 2000) {
+          console.log(hasCode);
+          chunk = hasCode ? chunk + '```' : chunk;
+          message.reply(chunk);
+          chunk = hasCode ? '```' : '';
+        }
+        if (stream[i].match(/```/g) !== null && stream[i].match(/```/g).length % 2 == 1) hasCode = !hasCode;
+        chunk += stream[i] + '\n'
+      }
+      if (chunk !== '') message.reply(chunk);
+    } else {
+      message.reply(response);
+    }
+    
   } catch (error) {
     console.log(error);
   }
